26513411da0a9b16e87c1a44008f1536a2e135a0
Pablo <gimenezpablonl@gmail.com>
2020-05-22 19:08:37 -0300

    update

--- server/index.js
+++ server/index.js
@@ -50,6 +50,8 @@ async function start() {
   // Give nuxt middleware to express
   app.use(nuxt.render)
   // Listen the server
+  console.log(port, host)
+  console.log(process.env.PORT)
   app.listen(port, host)
   consola.ready({
     message: `Server listening on http://${host}:${port}`,
